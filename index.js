3a0bcaad71af8f0489e4521ee94913ea749af168
shahrzadbst <shahrzadbaghestani@gmail.com>
2025-11-21 12:34:53 -0500

    not best practice, use of objects

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-var employee = { id: 1, name: 'Shahrzad' };
+var employee = { id: 1, name: 'Shahrzad', retire: function (date) { console.log(date); } };
--- index.ts
+++ index.ts
@@ -1,1 +1,1 @@
-let employee: {readonly id:Number, name:String}={id:1, name:'Shahrzad'}
+let employee:{readonly id:number, name:string, retire:(date:Date)=> void} ={id:1, name:'Shahrzad', retire:(date:Date)=> {console.log(date)}}
